3bcaaa214de73a7b6f378cf9554dbc47511d0793
Gautam Jakhar <108416604+realgautamjakhar@users.noreply.github.com>
2023-01-23 18:22:03 +0530

    final added cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cors = require("cors");
+
 const connectDB = require("./config/db");
-const PORT = 1338;
+const PORT = process.env.PORT || 1338;
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/user", require("./routes/user"));
